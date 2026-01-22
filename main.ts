abc8914c5e911a412f0d0c248a75e5e681be7cbd
teepakornsaijan-bit <teepakornsaijan@gmail.com>
2026-01-22 19:59:27 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-	
+    sprite.change(LedSpriteProperty.X, 1)
 })
+input.onButtonPressed(Button.B, function () {
+    sprite.change(LedSpriteProperty.X, -1)
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
 basic.forever(function () {
 	
 })
